17829659d9b7a0ada37ec7b9b38a582043645308
Ratan Kasnia <ratankasnia.img@gmail.com>
2024-06-06 13:21:48 +0530

    new

---
+++ src/context/Firebase.jsx
@@ -0,0 +1,13 @@
+import {createContext} from 'react'
+
+const FirebaseContext = createContext(null);
+
+
+export const FirebaseProvider = (props) => {
+    return (
+        <FirebaseContext.Provider>
+            {props.children}
+        </FirebaseContext.Provider>
+    )
+
+}
