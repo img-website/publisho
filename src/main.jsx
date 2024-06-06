e2857bf1e221efbcb13026975999933af929c3f8
Ratan Kasnia <ratankasnia.img@gmail.com>
2024-06-06 14:28:55 +0530

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { FirebaseProvider } from './context/Firebase.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { FirebaseProvider } from "./context/Firebase.jsx";
+import { NextUIProvider } from "@nextui-org/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FirebaseProvider>
-    <App />
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
     </FirebaseProvider>
   </React.StrictMode>
-)
+);
