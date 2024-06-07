e49090184021b6e4a68126ff7bcf49b00cf79bf0
mohitboharaimg <146337834+mohitboharaimg@users.noreply.github.com>
2024-06-07 10:33:50 +0530

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,213 @@
-import React from 'react'
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, User} from "@nextui-org/react";
+import {Input} from "@nextui-org/react";
+import { SearchIcon } from "../component/Icons";
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+        <Navbar maxWidth="xl" className="mx-auto w-full bg-indigo-50 shadow-xl">
+      <NavbarBrand>
+        {/* <AcmeLogo /> */}
+        <p className="font-bold text-inherit">BLOGERA</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4 " justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarItem>
+      <Input
+        label="Search"
+        isClearable
+        radius="lg"
+        classNames={{
+          label: "text-black/50 dark:text-white/90",
+          input: [
+            "bg-transparent",
+            "text-black/90 dark:text-white/90",
+            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+          ],
+          innerWrapper: "bg-transparent",
+          inputWrapper: [
+            "shadow-xl",
+            "bg-default-200/50",
+            "dark:bg-default/60",
+            "backdrop-blur-xl",
+            "backdrop-saturate-200",
+            "hover:bg-default-200/70",
+            "dark:hover:bg-default/70",
+            "group-data-[focus=true]:bg-default-200/50",
+            "dark:group-data-[focus=true]:bg-default/60",
+            "!cursor-text",
+          ],
+        }}
+        placeholder="Type to search..."
+        startContent={
+          <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+        }
+      />
+        </NavbarItem>
+      <NavbarContent justify="end">
+    
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+        <div className="flex gap-4 items-center">
+          
+      <Button 
+      isIconOnly color="primary"
+       variant="faded" 
+      aria-label="Take a photo"
+      className="sm:hidden"
+      >
+      <SearchIcon/>
+      </Button>
+    </div>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat" className="hidden sm:flex">
+            Sign Up
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+        <div className="flex items-center gap-4">
+      <Dropdown placement="bottom-end">
+        <DropdownTrigger>
+          <Avatar
+            isBordered
+            as="button"
+            className="transition-transform"
+            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+          />
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Profile Actions" variant="flat">
+          <DropdownItem key="profile" className="h-14 gap-2">
+            <p className="font-semibold">Signed in as</p>
+            <p className="font-semibold">zoey@example.com</p>
+          </DropdownItem>
+          <DropdownItem key="settings">
+            My Settings
+          </DropdownItem>
+          <DropdownItem key="team_settings">Team Settings</DropdownItem>
+          <DropdownItem key="analytics">
+            Analytics
+          </DropdownItem>
+          <DropdownItem key="system">System</DropdownItem>
+          <DropdownItem key="configurations">Configurations</DropdownItem>
+          <DropdownItem key="help_and_feedback">
+            Help & Feedback
+          </DropdownItem>
+          <DropdownItem key="logout" color="danger">
+            Log Out
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown> 
+    </div>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+    <div class="relative pb-12 bg-black xl:pt-24 sm:pb-16 lg:pb-32 xl:pb-48 2xl:pb-56">
+    
+
+    <div class="absolute inset-0">
+        <img class="object-cover w-full h-full" src="https://images.unsplash.com/photo-1610827776753-8c668b92d732?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    </div>
+
+    <div class="relative">
+        <div class="px-5 mx-auto max-w-7xl">
+            <div class="w-full lg:w-2/3 xl:w-1/2">
+                <h1 class="font-sans text-base font-normal tracking-tight text-white text-opacity-70">Master the basics of baking</h1>
+                <p class="mt-6 tracking-tighter text-white">
+                    <span class="font-sans font-normal italic text-7xl">Unlock the World of </span><br />
+                    <span class="font-serif italic font-normal text-8xl">blogera</span>
+                </p>
+                <p class="mt-12 font-sans text-base font-normal leading-7 text-white text-opacity-70">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu penatibus pellentesque dolor consequat ligula egestas massa gravida. Porttitor venenatis enim praesent.</p>
+
+                <div class="flex items-center mt-5 space-x-3 sm:space-x-4">
+                    {/* <a
+                        href="#"
+                        title=""
+                        class="
+                            inline-flex
+                            items-center
+                            justify-center
+                            px-5
+                            py-2
+                            font-sans
+                            text-base
+                            font-semibold
+                            transition-all
+                            duration-200
+                            border-2 border-transparent
+                            rounded-full
+                            sm:leading-8
+                            bg-white
+                            sm:text-lg
+                            text-black
+                            hover:bg-opacity-90
+                            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:ring-offset-secondary
+                        "
+                        role="button"
+                    >
+                        Get started
+                    </a> */}
+
+                    <a
+                        href="#"
+                        title=""
+                        class="
+                            inline-flex
+                            items-center
+                            justify-center
+                            px-5
+                            py-2
+                            font-sans
+                            text-base
+                            font-semibold
+                            transition-all
+                            duration-200
+                            bg-transparent
+                            border-2
+                            rounded-full
+                            sm:leading-8
+                            text-white
+                            border-primary
+                            hover:bg-white
+                            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary
+                            hover:text-black
+                            sm:text-lg
+                            focus:ring-offset-secondary
+                        "
+                        role="button"
+                    >
+                        <svg class="w-6 h-6 mr-2" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.0416 4.9192C7.37507 4.51928 6.5271 4.99939 6.5271 5.77669L6.5271 18.2232C6.5271 19.0005 7.37507 19.4806 8.0416 19.0807L18.4137 12.8574C19.061 12.469 19.061 11.5308 18.4137 11.1424L8.0416 4.9192Z" />
+                        </svg>
+                       Get Started
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </>
   )
 }
+
 
 export default Home
